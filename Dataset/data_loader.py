refractor code, improved visualization, fixed weighted adjacency matrix normalization

Dataset/data_loader.py
@@ -1,10 +1,31 @@
 import os
 import sys
 from pathlib import Path
+from typing import Tuple
 
 import rdata # to read .RData files
 import numpy as np
 import pandas as pd
+
+
+def get_data(data_path: Path, data_type: str)-> Tuple[np.array, pd.DataFrame]:
+    """Return the adjacency matrices and the corresponding features
+    Args:
+        data_path (Path): path to the dataset folder
+        data_type (str): type of the data (e.g., 'balanced', 'unbalanced')
+    Returns:
+        A (np.ndarray): adjacency matrices for the specified data type
+        data (pd.DataFrame): features and labels for the specified data type
+    """
+    # Check if converted data is already available
+    if not os.path.exists(data_path/f'converted/{data_type}'):
+        print('Converted data not available, converting...')
+        convert_rdata(data_path/f'raw/sim_data_{data_type}.RData')
+        print('Done')
+    
+    A, data = load_data(data_path/f'converted/{data_type}')
+    return A, data
+
 
 def load_data(path: Path):
     """Load adjacency matrices from .npy file and data from .csv file
@@ -19,30 +40,11 @@ def load_data(path: Path):
     
     # Load data
     data = pd.read_csv(f'{path}/data.csv')
-    
-    # Convert data to numpy array
+
     return A, data
-
-
-def get_data(data_path: Path, data_type: str):
-    # Check if converted data is already available
-    if not os.path.exists(data_path/f'converted/{data_type}'):
-        print('Converted data not available, converting...')
-        convert_rdata(data_path/f'raw/sim_data_{data_type}.RData')
         
-    save_path = os.path.join(Path.cwd() / f'data/preprocessed/{data_type}')
-    if os.path.exists(save_path / 'adj_mtrx.npy') and os.path.exists(save_path / 'data.csv'):
-        return np.load(save_path / 'adj_mtrx.npy'), pd.read_csv(save_path / 'data.csv')
-    
-    # If not, convert the .RData file
-    file_path = os.path.join(data_path, f'{data_type}.RData')
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"No RData file found at {file_path}")
-    
-    return convert_rdata(file_path)
-    
 
-def convert_rdata(file_path) -> (np.array, pd.DataFrame): # type: ignore
+def convert_rdata(file_path) -> Tuple[np.array, pd.DataFrame]: 
     """Read a RData file and return an adjacency matrices and data
     Args:
         path (str): path to the RData file    
@@ -50,35 +52,18 @@ def convert_rdata(file_path) -> (np.array, pd.DataFrame): # type: ignore
         adj_matrices (np.array): adjacency matrices for each graph
         data (list[pd.DataFrame]): features and labels for each graph
     """
+    # Required for the 'rdata' module
     assert sys.version_info >= (3, 9), f"This script requires Python 3.9 or later. Current Python version: {'.'.join([str(x) for x in sys.version_info[:3]])}"
 
+    # Extract the necessary data from the RData file
     parsed_data = rdata.parser.parse_file(file_path)
     converted_data = rdata.conversion.convert(parsed_data, default_encoding="utf8")
+    A, data = converted_data['Adj_matrices'], converted_data['sim_data1']
     
+    # Create a save path for the converted data 
     save_path = os.path.join(Path.cwd() / f'data/preprocessed/{data_type}')
     os.makedirs(save_path, exist_ok=True)
     
-    # load the datasets
-    data_path = os.path.join(Path.cwd() / 'data/raw/')
-    
-    # store the names of the files in the folder
-    data_names = os.listdir(data_path)
-    
-    for name in data_names:
-        file_path = os.path.join(data_path, name)
-        
-        # get the type of the data
-        data_type = name.split('.')[0].split('_')[-1]
-        
-        save_path = os.path.join(Path.cwd() / f'data/preprocessed/{data_type}')
-        os.makedirs(save_path, exist_ok=True)
-        
-        # load the data fron .RData file
-        A, data = read_rdata(file_path)
-        print(save_path)
-        # save adjacency matrices
-        np.save(save_path + '/Adj_mtrx.npy', A)
-        # save data
-        data.to_csv(save_path + '/data.csv', index=False)
-    
-    return np.array(converted_data['Adj_matrices']), converted_data['sim_data1']
+    # save adjacency matrices and data
+    np.save(save_path + '/Adj_mtrx.npy', A)
+    data.to_csv(save_path + '/data.csv', index=False)